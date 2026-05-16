45dc16fabdc2ddd9b470ffdbeee80ac0e215255f
pramila chaudhary <chaudharyparmila75@gmail.com>
2026-05-16 15:42:28 +0545

    todo

---
+++ todo_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from todo_app.models import Todo
+
+# Create your views here.
+
+def todo_list(request):
+    
+    todos=Todo.objects.all()
+    return render(
+        request,
+        "todo_list.html",
+        {"todos":todos},
+        )
